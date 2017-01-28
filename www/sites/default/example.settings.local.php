1854e0eb3678d54aa297e6dbc0d4025b6af82aa3
Florent Torregrosa <florent.torregrosa@gmail.com>
2017-01-28 19:38:53 +0100

    Put cache config into setting.php instead of settings.local.php

--- www/sites/default/example.settings.local.php
+++ www/sites/default/example.settings.local.php
@@ -18,19 +18,6 @@ $settings['trusted_host_patterns'] = array(
 // Piwik.
 $config['piwik.settings']['url_http'] = 'http://127.0.0.1:8083/';
 
-// External cache.
-if (file_exists(__DIR__ . '/.cache_activated')) {
-  $settings['redis.connection']['interface'] = 'PhpRedis';
-  $settings['redis.connection']['host']      = 'redis';
-  $settings['cache']['default'] = 'cache.backend.redis';
-
-  // Always set the fast backend for bootstrap, discover and config, otherwise
-  // this gets lost when redis is enabled.
-  $settings['cache']['bins']['bootstrap'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['discovery'] = 'cache.backend.chainedfast';
-  $settings['cache']['bins']['config'] = 'cache.backend.chainedfast';
-}
-
 if (file_exists(__DIR__ . '/../development.settings.php')) {
   include __DIR__ . '/../development.settings.php';
 }
