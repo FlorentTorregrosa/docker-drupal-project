17253b9177c24a837ad70fc0384e85bf77ac2b04
Florent Torregrosa <florent.torregrosa@gmail.com>
2020-02-13 21:31:42 +0100

    Update update_translations task to not reference undefined variables.

--- scripts/tasks/update_translations.sh
+++ scripts/tasks/update_translations.sh
@@ -10,4 +10,4 @@ $DRUSH "${DRUPAL_SITE_DEFAULT_DRUSH_ALIAS}" locale:update
 
 #echo -e "${COLOR_LIGHT_GREEN}Import custom translations.${COLOR_NC}"
 #$DRUSH "${DRUPAL_SITE_DEFAULT_DRUSH_ALIAS}" pm:enable drush_language -y
-#$DRUSH "${DRUPAL_SITE_DEFAULT_DRUSH_ALIAS}" language:import:translations --langcode="${DEFAULT_LANGUAGE}" $APP_PATH/profiles/custom/$PROFILE/translations/$PROFILE.po
+#$DRUSH "${DRUPAL_SITE_DEFAULT_DRUSH_ALIAS}" language:import:translations --langcode="${DRUPAL_SITE_DEFAULT_DEFAULT_LANGUAGE}" $APP_PATH/profiles/custom/${DRUPAL_SITE_DEFAULT_INSTALLATION_PROFILE}/translations/${DRUPAL_SITE_DEFAULT_INSTALLATION_PROFILE}.po
