dbe4aaaf33a67c21ff9df1deb3b9a0fa4f66f86c
Florent Torregrosa <florent.torregrosa@gmail.com>
2020-04-27 16:13:25 +0200

    Rename development modules task.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@
 . ${SCRIPTS_PATH}/tasks/install_drupal.sh
 . ${SCRIPTS_PATH}/tasks/update_database.sh
 #. ${SCRIPTS_PATH}/tasks/import_configuration.sh
-. ${SCRIPTS_PATH}/tasks/development_modules.sh
+. ${SCRIPTS_PATH}/tasks/enable_development_modules.sh
 . ${SCRIPTS_PATH}/tasks/update_translations.sh
 . ${SCRIPTS_PATH}/tasks/import_default_content.sh
 . ${SCRIPTS_PATH}/tasks/flush_cache.sh
